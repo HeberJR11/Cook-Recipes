b8c0453808c420bc8dee9c98cac8e0496e23196f
HeberJR11 <heberedu_17@hotmail.com>
2022-08-23 16:57:11 -0700

    Primer commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by MAC on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
